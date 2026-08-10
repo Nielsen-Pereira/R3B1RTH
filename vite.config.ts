be2bd27c66e9eafb55a791d1107ecebe15935b3f
Nielsen PEREIRA <nielsenpereira@outlook.com>
2026-08-10 18:55:13 +0200

    Add vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/R3B1RTH/',
-  build: { outDir: 'dist', sourcemap: true },
-  server: { port: 3000, open: true, cors: true },
-  preview: { port: 4173 },
+  root: '.',
+  publicDir: 'public'
 });
