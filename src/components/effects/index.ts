74619481f4e6dbf8538a167a1b29f7081fe80cef
Nielsen PEREIRA <nielsenpereira@outlook.com>
2026-08-10 19:04:10 +0200

    Add src/components/effects/index.ts

--- src/components/effects/index.ts
+++ src/components/effects/index.ts
@@ -1,1 +1,5 @@
+export { default as Distortion } from './Distortion';
+export { default as PCF } from './PCF';
+export { default as Compressor } from './Compressor';
+export { default as Delay } from './Delay';
 export { default as EffectsSection } from './EffectsSection';
