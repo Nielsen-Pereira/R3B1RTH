51b4426b0f8939a6dcf04b0070a52963bcc729db
Nielsen PEREIRA <nielsenpereira@outlook.com>
2026-08-10 20:27:10 +0200

    Update configuration files (package.json, tsconfig, vite, eslint, gitignore)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,14 +10,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    '@typescript-eslint/no-explicit-any': 'warn',
   },
 };
