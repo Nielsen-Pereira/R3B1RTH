c1dc7adaa1fc868e43aeb0327dae86e4ea0859cb
Nielsen PEREIRA <nielsenpereira@outlook.com>
2026-08-11 23:31:20 +0200

    feat: Update stores/index.ts to include songStore (R3B-90)

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,7 +1,9 @@
-/**
- * Stores Index - Batch 1 Development
- * Centralized exports for all Zustand stores
- */
-
-export { useSongStore, getCurrentSong, isRecording, isPlaying } from './songStore';
-export { useAudioEffectsStore, getEffectConfig, getInstrumentRouting, getMasterRouting, isEffectUsed } from './audioEffectsStore';
+// Export all stores
+export { default as useAudioStore } from './audioStore';
+export { default as usePatternStore } from './patternStore';
+export { default as useSequencerStore } from './sequencerStore';
+export { default as useSongStore } from './songStore';
+export { default as useAudioEffectsStore } from './audioEffectsStore';
+export { default as useTb303Store } from './tb303Store';
+export { default as useTr808Store } from './tr808Store';
+export { default as useTr909Store } from './tr909Store';
