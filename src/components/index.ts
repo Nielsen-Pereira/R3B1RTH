9d166598d676849129ebfcf5825ecefbd58cedf4
Nielsen PEREIRA <nielsenpereira@outlook.com>
2026-08-11 12:18:49 +0200

    feat: update components barrel export with instrument components (Batch 2)

---
+++ src/components/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Components Index - Batch 2 Development
+ * Centralized exports for all components
+ */
+
+export { default as SongMode } from './SongMode';
+export { default as AudioEffectsControls } from './AudioEffectsControls';
+export { default as PatternEditor } from './PatternEditor';
+export { default as TransportControls } from './TransportControls';
+export { default as TB303 } from './TB303';
+export { default as TR808 } from './TR808';
+export { default as TR909 } from './TR909';
