266fc4601c98d10a3367042181f40f6a8257ef31
Nielsen PEREIRA <nielsenpereira@outlook.com>
2026-08-11 12:08:54 +0200

    feat: update components barrel export with TransportControls (Batch 1)

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,4 @@
 export { default as SongMode } from './SongMode';
 export { default as AudioEffectsControls } from './AudioEffectsControls';
 export { default as PatternEditor } from './PatternEditor';
+export { default as TransportControls } from './TransportControls';
