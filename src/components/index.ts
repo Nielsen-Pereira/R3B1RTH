8e3c1a304190ae60f734d7efe523f93e8463692b
Nielsen PEREIRA <nielsenpereira@outlook.com>
2026-08-11 11:48:28 +0200

    feat: add components barrel export (Batch 1)

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Components Index - Batch 1 Development
+ * Centralized exports for all components
+ */
+
+export { default as SongMode } from './SongMode';
+export { default as AudioEffectsControls } from './AudioEffectsControls';
+export { default as PatternEditor } from './PatternEditor';
