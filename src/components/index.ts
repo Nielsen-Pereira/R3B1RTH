642271e029bbf3f3d6a98986199f6def0c290eb7
Nielsen PEREIRA <nielsenpereira@outlook.com>
2026-08-12 14:05:35 +0200

    feat: Update components/index.ts to export EffectsRouting (Batch 7)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,17 +1,17 @@
-/**
- * Components Index - Batch 3 Development
- * Centralized exports for all components
- */
-
-export { default as SongMode } from './SongMode';
-export { default as AudioEffectsControls } from './AudioEffectsControls';
-export { default as PatternEditor } from './PatternEditor';
+// Export all components
+export { default as App } from './App';
 export { default as TransportControls } from './TransportControls';
+export { default as PatternSelector } from './PatternSelector';
+export { default as Sequencer } from './Sequencer';
+export { default as Mixer } from './Mixer';
+export { default as EffectsPanel } from './EffectsPanel';
+export { default as EffectsRouting, MasterEffectsRouting } from './EffectsRouting';
 export { default as TB303 } from './TB303';
 export { default as TR808 } from './TR808';
 export { default as TR909 } from './TR909';
-export { default as MainSequencer } from './MainSequencer';
-export { default as PatternManager } from './PatternManager';
-export { default as PatternSelector } from './PatternSelector';
-
+export { default as SongMode } from './SongMode';
+export { default as PatternChaining } from './PatternChaining';
 export { default as PatternActions } from './PatternActions';
+export { default as AudioEffectsControls } from './AudioEffectsControls';
+export { default as AutomationControls } from './AutomationControls';
+export { default as AdvancedSequencerControls } from './AdvancedSequencerControls';
