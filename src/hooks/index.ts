ac65d4a60cabfe4f70d79e91378f1eff7399ad83
Nielsen PEREIRA <nielsenpereira@outlook.com>
2026-08-13 18:43:02 +0200

    Update hooks/index.ts to include all exports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,11 @@
 /**
- * Hooks Index - Batch 1 Development
- * Centralized exports for all custom hooks
+ * Hooks exports
  */
 
+export { default as useSongStore } from './useSongStore';
+export { default as usePatternStore } from './usePatternStore';
+export { default as useAutomationStore } from './useAutomationStore';
+export { default as useAudioEffectsStore } from './useAudioEffectsStore';
+export { default as useAdvancedSequencer } from './useAdvancedSequencer';
 export { default as useAutomation } from './useAutomation';
+export { default as useFileManager } from './useFileManager';
