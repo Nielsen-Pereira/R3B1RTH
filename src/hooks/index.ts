2a01b0651b54293670be4e9bf2d2705b63741b7d
Nielsen PEREIRA <nielsenpereira@outlook.com>
2026-08-16 02:30:26 +0200

    feat(R3B-88): Update hooks/index.ts to export useMods

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -11,3 +11,4 @@ export { default as useAutomation } from './useAutomation';
 export { default as useFileManager } from './useFileManager';
 export { default as useAudio } from './useAudio';
 export { default as useSequencer } from './useSequencer';
+export { default as useMods } from './useMods';
