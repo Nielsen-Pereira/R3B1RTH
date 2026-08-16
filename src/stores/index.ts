6033d3ee830b5de4641f4ba5666e425f38dbacbf
Nielsen PEREIRA <nielsenpereira@outlook.com>
2026-08-16 03:24:09 +0200

    feat(R3B-114): Export fileManagerStore from stores index

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,12 +1,10 @@
-/**
- * Stores Index - Batch 3 Development
- * Centralized exports for all Zustand stores
- */
-
-export { useSongStore, getCurrentSong, isRecording, isPlaying } from './songStore';
-export { useAudioEffectsStore, getEffectConfig, getInstrumentRouting, getMasterRouting, isEffectUsed } from './audioEffectsStore';
-export { useTransportStore, getTransportInfo, beatsToSeconds, secondsToBeats, getBeatDuration, getSixteenthNoteDuration, createMetronomeClick } from './transportStore';
-export { useTB303Store, getTB303State, isTB303Enabled, isTB303Muted, isTB303Solo, getTB303Volume, getTB303Parameter, TB303_PRESETS, applyTB303Preset } from './tb303Store';
-export { useTR808Store, getTR808State, isTR808Enabled, isTR808Muted, isTR808Solo, getTR808Volume, getDrumState, isDrumEnabled, getDrumParameter, TR808_PRESETS, applyTR808Preset } from './tr808Store';
-export { useTR909Store, getTR909State, isTR909Enabled, isTR909Muted, isTR909Solo, getTR909Volume, getTR909DrumState, isTR909DrumEnabled, getTR909DrumParameter, TR909_PRESETS, applyTR909Preset } from './tr909Store';
-export { usePatternStore, getClipboardPattern, hasClipboardContent, getPatternState, getCurrentPattern, getPatternsByInstrument, getAllPatterns, getPatternCount, isPatternMode, isSongMode, getCurrentInstrument, getPatternLength, getSwing, getShuffle, getPatternById, getPatternByIndex, getActivePatterns, getEmptyPatterns, getPatternStats, PATTERN_PRESETS, applyPatternPreset } from './patternStore';
+export * from './audioEffectsStore';
+export * from './fileManagerStore';
+export * from './modsStore';
+export * from './patternChainingStore';
+export * from './patternStore';
+export * from './songStore';
+export * from './tb303Store';
+export * from './tr808Store';
+export * from './tr909Store';
+export * from './transportStore';
